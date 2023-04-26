ce7e4319c5431d37f5c9b5c56cb751bda3eca337
Erick Moore <erick.moore@gmail.com>
2023-04-25 21:00:10 -0400

    updated script

--- scripts/simple.sh
+++ scripts/simple.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-url="http://20.122.225.76:5000/"
+url="$1:5000"
+vote_option="$2"
 count=10
+
+if [ "$vote_option" = "cat" ]; then
+  vote="vote=a"
+elif [ "$vote_option" = "dog" ]; then
+  vote="vote=b"
+else
+  echo "Invalid vote option. Please use 'cat' or 'dog'."
+  exit 1
+fi
 
 echo "Repeatedly voting for cat"
 
 for i in $(seq 1 $count); do
   echo "Vote $i of $count"
-  curl -sSL -o /dev/null -X POST -d "vote=a" "$url"
+  curl -sSL -o /dev/null -X POST -d "$vote" "$url"
   sleep 1
 done
 
